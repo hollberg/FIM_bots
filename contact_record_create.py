664f21e240e3c353d38f5a492e0c50ec07ffbc26
Mitchell.Hollberg <mhollberg@gmail.com>
2018-09-27 14:46:13 -0400

    Build contact records (contact_record_create.py). Also added and updated helper function (text_clean, etc) in gui_tools.py

--- contact_record_create.py
+++ contact_record_create.py
@@ -13,6 +13,7 @@ import gui_tools as gt
 
 # External Libraries
 import pyautogui as bot
+import pandas as pd
 
 IMG_DIR = 'img/contact_record_create/'
 
@@ -40,7 +41,8 @@ def new_contact(fims_id:str,
         new_record_xy = bot.locateCenterOnScreen(IMG_DIR + 'new_record.png')
 
     # Click the "New" icon (piece of paper) to launch new contact window
-    gt.click_pic(IMG_DIR + 'new_record.png')
+    bot.click(new_record_xy[0], new_record_xy[1])
+    time.sleep(2)
 
     # New Contact record window launches with "Grant" field highlighted.
     # tab twice to get to FIMS ID
@@ -80,7 +82,7 @@ def new_contact(fims_id:str,
     gt.tab_then_type(1, contact_comment)
 
     # Tab to get to grant
-    gt.tab_then_type(1, contact_grant)
+    # gt.tab_then_type(1, contact_grant)
 
     # Click the save (floppy disk) icon
     gt.click_pic(IMG_DIR + 'new_contact_save_icon.png')
@@ -89,28 +91,65 @@ def new_contact(fims_id:str,
     time.sleep(1)
 
     # Select "File" -> Close Tab via "Alt" -> F -> F -> <enter>
-    bot.press(['alt', 'f', 'f', 'enter', 'enter'], interval=.4)
+    bot.press(['alt', 'f', 'f', 'enter'], interval=1)
+    bot.hotkey('alt', 'f4')
 
     # Cleanup - Move mouse to lower left corner)
     bot.moveTo(0, bot.size()[1])
 
 
+def get_contact_data(file_path):
+    """
+    Read Excel file with contact import data
+    :param file_path:
+    :return:
+    """
+    df_contacts = pd.read_excel(file_path)
+    # print(df_contacts.columns)
+    return df_contacts
+
 def main():
     time.sleep(2)
-    new_contact(fims_id = '52545', contact_type='COTH', tickle_date='1/1/2018',
-                solicitor='CMTG', staff='MH', fund='2004',
-                contact_comment = r"""Now there buddy!
-                What's your name?
-                
-                I just skipped two lines.
-                Now I'm done""")
 
-    new_contact(fims_id='52545', contact_type='COTH', tickle_date='1/1/2018',
-                solicitor='CMTG', staff='MH', fund='2004',
-                contact_comment=r"""Now there buddy!
-                What's your name?
+    contact_filepath = r'P:\03_FinanceOperations\InformationManagement\Databases\FIM_Bots_data'
+    contact_file = r'FIM_Bots_contacts_20180926.xlsx'
+    df_contacts = get_contact_data(contact_filepath + r'\\' + contact_file)
 
-                I just skipped two lines.
-                Now I'm done""")
+    for index, row in df_contacts.iterrows():
+        fims_id = str(row['FIMSID'])
+        staff = row['StaffCode']
+        contact_comment = '<Cleanup of Donor "Comment" field - MMH - 20180927>' + '\n' + \
+                          gt.clean_text(row['Comment']) #Strip special chars
+        contact_type = row['CntctType']
+        eff_date = pd.to_datetime(row['EffDate'])
+        contact_date = f'{str(eff_date.month)}/{str(eff_date.day)}/{str(eff_date.year)}'
+
+
+        print(f'preparing to load for {fims_id}')
+        # print(contact_comment)
+
+        new_contact(fims_id=fims_id, staff=staff, contact_type=contact_type,
+                    contact_date=contact_date, contact_comment=contact_comment)
+
+        time.sleep(2)
+
+
+
+
+    # new_contact(fims_id = '52545', contact_type='COTH', tickle_date='1/1/2018',
+    #             solicitor='CMTG', staff='MH', fund='2004',
+    #             contact_comment = r"""Now there buddy!
+    #             What's your name?
+    #
+    #             I just skipped two lines.
+    #             Now I'm done""")
+    #
+    # new_contact(fims_id='52545', contact_type='COTH', tickle_date='1/1/2018',
+    #             solicitor='CMTG', staff='MH', fund='2004',
+    #             contact_comment=r"""Now there buddy!
+    #             What's your name?
+    #
+    #             I just skipped two lines.
+    #             Now I'm done""")
 
 main()
--- gui_tools.py
+++ gui_tools.py
@@ -1,5 +1,6 @@
 import pyautogui as bot
 import time
+import re
 
 # Issues with 4k monitors? Per comments at
 # https://github.com/asweigart/pyautogui/issues/33
@@ -7,11 +8,15 @@ from ctypes import windll
 user32 = windll.user32
 user32.SetProcessDPIAware()
 
-def click_pic(path_to_photo, sleep = 0, logging = False):
+def click_pic(path_to_photo, sleep = 0, logging = False, region = None):
     """given the filepath to a lossless imagage format (like *.png), find the
     (x,y) coordinates of that image on the screen, and click the mouse there """
-    print('Looking for location of ' + path_to_photo)
-    xy = bot.locateCenterOnScreen(path_to_photo, grayscale=True)
+    if logging:
+        print('Looking for location of ' + path_to_photo)
+    if region:
+        xy = bot.locateCenterOnScreen(path_to_photo, region=region, grayscale=True)
+    else:
+        xy = bot.locateCenterOnScreen(path_to_photo, grayscale=True)
     if xy is None:
         print(path_to_photo + ' image not found')
         return None
@@ -39,3 +44,56 @@ def screencapt(output_file, region=(0,0, 1920, 1080)):
     bot.screenshot(output_file, region=region)
     return True
 
+def get_screenres():
+    screen_res = str(bot.size()[0]) + 'x' + str(bot.size()[1])
+    return screen_res
+
+
+def track_mouse():
+   """
+   Utility function to keep running log of current mouse X,Y position
+   :return:
+   """
+   for i in range(1999):
+      time.sleep(.1)
+      x,y = bot.position()
+      print(str(x) + ", " + str(y))
+
+
+def press_x_times(input_key:str, repetitions:int, interval=.2):
+    """
+    Press 'key' characters 'repetitions' number of times
+    :param input_key: String, key(s) to press
+    :param repetitions: Int, number of times to repeat
+    :return: None
+    """
+    for _ in range(repetitions):
+        bot.press(input_key)
+        time.sleep(interval)
+
+    return None
+
+def tab_then_type(tab_count:int = 1, input_value:str=None):
+    """
+    Convenience script. Tab "tab_count" times to advance selected cell.
+    Then, type in the 'input_value' into selected field
+    :param tab_count: Number of times to 'tab' and advance selected input
+    :param input_value: Value to paste into selected cell
+    :return: None
+    """
+    press_x_times('tab', tab_count, interval=1)
+    if input_value:
+        bot.typewrite(input_value)
+
+    return None
+
+
+def clean_text(input_text:str)->str:
+    """
+    Given a text string, clean out all non-printing characters except 'enter'
+    :param input_text:
+    :return: Cleaned text
+    """
+    re_chars_and_hard_return = re.compile('[^A-Za-z0-9 !@#$%^&*(){}[]/<>-+=_;:"\',.\n]')
+    cleaned_text = re_chars_and_hard_return.sub('',input_text)
+    return cleaned_text
